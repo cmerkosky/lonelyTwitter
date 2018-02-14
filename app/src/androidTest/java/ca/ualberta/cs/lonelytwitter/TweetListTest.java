820f99ed5aca954cd8081923ed6adea2cf02fcd9
Cole Merkosky <cjmerkos@ualberta.ca>
2018-02-13 18:33:30 -0700

    Finished up for lab portion

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -2,6 +2,8 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import java.util.ArrayList;
+
 /**
  * Created by cole on 13/02/18.
  */
@@ -19,15 +21,30 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
         tweets.add(tweet);
         assertTrue(tweets.hasTweet(tweet));
 
+        try{
+            tweets.add(tweet);
+        } catch (IllegalArgumentException e){
+            assertTrue(Boolean.TRUE);
+        }
+
     }
 
     public void testHasTweet(){
 
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("Hello");
+        Tweet tweet2 = new NormalTweet("Hello");
+        tweet2.setDate(tweet.getDate());
+
         assertFalse(tweets.hasTweet(tweet));
         tweets.add(tweet);
         assertTrue(tweets.hasTweet(tweet));
+
+        try{
+            tweets.add(tweet2);
+        } catch (IllegalArgumentException e){
+            assertTrue(Boolean.TRUE);
+        }
     }
 
     public void testGetTweet(){
@@ -48,5 +65,38 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
         tweets.add(tweet);
         tweets.delete(tweet);
         assertFalse(tweets.hasTweet(tweet));
+    }
+
+    public void testGetTweets(){
+        TweetList tweets = new TweetList();
+        Tweet tweet1 = new NormalTweet("Hello");
+        Tweet tweet2 = new NormalTweet("Test");
+        Tweet tweet3 = new NormalTweet("Bye");
+        tweets.add(tweet1);
+        tweets.add(tweet2);
+        tweets.add(tweet3);
+
+        ArrayList<Tweet> sortedTweets = tweets.getTweets();
+
+        for(int i = 0; i < sortedTweets.size()-1; i++){
+            assertFalse(sortedTweets.get(i).getDate().after(sortedTweets.get(i+1).getDate()));
+        }
+    }
+
+    public void testGetCount(){
+        TweetList tweets = new TweetList();
+        Tweet tweet1 = new NormalTweet("Hello");
+        Tweet tweet2 = new NormalTweet("Test");
+        Tweet tweet3 = new NormalTweet("Bye");
+        assertTrue(tweets.getCount() == 0);
+
+        tweets.add(tweet1);
+        assertTrue(tweets.getCount() == 1);
+
+        tweets.add(tweet2);
+        assertTrue(tweets.getCount() == 2);
+
+        tweets.add(tweet3);
+        assertTrue(tweets.getCount() == 3);
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,6 +1,8 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by cole on 13/02/18.
@@ -9,12 +11,32 @@ import java.util.ArrayList;
 public class TweetList {
     private ArrayList<Tweet> tweets = new ArrayList<>();
 
+    class SortByDate implements Comparator<Tweet>{
+        public int compare (Tweet a, Tweet b){
+            if(a.getDate().before(b.getDate())){
+                return 1;
+            }
+            else if (a.getDate().after(b.getDate())){
+                return -1;
+            }
+            return 0;
+        }
+    }
+
     public void add(Tweet tweet){
+        if (hasTweet(tweet)){
+            throw new IllegalArgumentException();
+        }
         tweets.add(tweet);
     }
 
     public boolean hasTweet(Tweet tweet){
-        return tweets.contains(tweet);
+        for(int i = 0; i < tweets.size(); i++){
+            if (tweets.get(i).equals(tweet)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public Tweet getTweet(int index){
@@ -23,5 +45,19 @@ public class TweetList {
 
     public void delete(Tweet tweet){
         tweets.remove(tweet);
+    }
+
+    public ArrayList<Tweet> getTweets(){
+        ArrayList<Tweet> sorted_tweets = (ArrayList<Tweet>) tweets.clone();
+        Collections.sort(sorted_tweets, new SortByDate());
+        System.out.println(sorted_tweets);
+        return sorted_tweets;
+    }
+
+    public int getCount(){
+        if (tweets == null){
+            return 0;
+        }
+        return tweets.size();
     }
 }
